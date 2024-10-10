5eada70cee8b0b5393272de045bfc0cb57468a21
Komronbek Olimov <116647964+komronbeko@users.noreply.github.com>
2024-10-10 17:22:24 +0100

    First commit

---
+++ week1.js
@@ -0,0 +1,31 @@
+// Addition
+
+// const num1 = 2;
+// const num2 = 4;
+// const sum = num1 + num2;
+
+// console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+// Substraction
+
+// const sub1 = 6;
+// const sub2 = 3;
+// const subSum = sub1 - sub2;
+
+// console.log("The substraction of " + sub1 + " and " + sub2 + " is " + subSum);
+
+// Multiplication
+
+// const mult1 = 5;
+// const mult2 = 4;
+// const multSum = mult1 * mult2;
+
+// console.log("Multiplication of " + mult1 + " and " + mult2 + " is " + multSum);
+
+// Division
+
+const div1 = 40;
+const div2 = 8;
+const divSum = div1 / div2;
+
+console.log("Division of " + div1 + " and " + div2 + " is " + divSum);
